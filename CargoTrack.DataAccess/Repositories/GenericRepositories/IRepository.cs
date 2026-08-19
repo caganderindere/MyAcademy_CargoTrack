5675d3c0471b20db193aadcd8180d39765e44c50
Çağan Derindere <cederindere@hotmail.com>
2026-08-19 09:53:19 +0300

    Initial commit

---
+++ CargoTrack.DataAccess/Repositories/GenericRepositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using CargoTrack.Entity.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoTrack.DataAccess.Repositories.GenericRepositories
+{
+	public interface IRepository<TEntity> where TEntity : BaseEntity
+	{
+		Task<List<TEntity>> GetAllAsync();
+		Task<TEntity> GetByIdAsync(Guid id);
+
+		Task CreateAsync(TEntity entity);
+		Task UpdateAsync(TEntity entity);
+		Task DeleteAsync(TEntity entity);
+
+
+	}
+}
---
+++ CargoTrack.Entity/Entities/Cargo.cs
@@ -0,0 +1,19 @@
+﻿using CargoTrack.Entity.Entities.Common;
+using CargoTrack.Entity.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoTrack.Entity.Entities
+{
+	public class Cargo : BaseEntity
+	{
+		public string TrackCode { get; set; }
+		public DateTime ShipmentDate { get; set; }
+		public DateTime ArrivalDate { get; set; }
+		public double Weight { get; set; }
+		public CargoType CargoType { get; set; }
+	}
+}
